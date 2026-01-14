79d8ceb7b88864faeb4c1e0b83402d2f010e57c0
Den Kezlia <den.kezlia@gmail.com>
2026-01-14 13:20:39 +0100

    Updated git actions for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/login-evil-martians',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   }
